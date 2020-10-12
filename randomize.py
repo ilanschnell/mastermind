f29991f016aab131aa58bbb8bcd323a9eaba807f
Ilan Schnell <ilanschnell@gmail.com>
2020-10-12 11:17:47 -0500

    avoid duplicate code

--- randomize.py
+++ randomize.py
@@ -3,25 +3,9 @@ import random
 import itertools
 from functools import lru_cache
 
+import mastermind
+from mastermind import COLORS, get_response, score, possible, responses
 
-COLORS = 'BGKRWY'  # Blue, Green, blacK, Red, White, Yellow
-SECRET = None
-
-def get_response(g):
-    if SECRET is None:
-        inp = input("%s: " % g)
-        return inp.count('+'), inp.count('-')
-    return score(SECRET, g)
-
-@lru_cache(1 << 20)
-def score(a, b):
-    matches = sum(x == y for x, y in zip(a, b))
-    return matches, sum(min(a.count(j), b.count(j)) for j in COLORS) - matches
-
-possible = [''.join(p) for p in itertools.product(COLORS, repeat=4)]
-responses = [(right, wrong) for right in range(5)
-             for wrong in range(5 - right)]
-responses.remove((3, 1))  # len(responses) = 14
 
 def guess(S):
     if len(S) == len(possible):  # first
@@ -60,13 +44,14 @@ if __name__ == '__main__':
         sys.exit("Usage: %s [secret]" % sys.argv[0])
 
     if len(sys.argv) == 2:
-        SECRET = sys.argv[1]
-        if SECRET == '-':
-            SECRET = ''.join(random.choices(COLORS, k=4))
-            print("secret: %s" % SECRET)
+        mastermind.SECRET = sys.argv[1]
+        if mastermind.SECRET == '-':
+            mastermind.SECRET = ''.join(random.choices(COLORS, k=4))
+            print("secret: %s" % mastermind.SECRET)
             solve()
             sys.exit()
-        if len(SECRET) != 4 or set(SECRET) - set(COLORS):
-            sys.exit("is not a well-formed Mastermind code: %r" % SECRET)
+        if len(mastermind.SECRET) != 4 or set(mastermind.SECRET) - set(COLORS):
+            sys.exit("is not a well-formed Mastermind code: %r" %
+                     mastermind.SECRET)
 
     solve()
