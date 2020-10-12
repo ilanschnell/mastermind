fbecfb7f2f7474ce366cb1122e98ae207bafd944
Ilan Schnell <ilanschnell@gmail.com>
2020-10-11 21:34:29 -0500

    add new file

---
+++ randomize.py
@@ -0,0 +1,70 @@
+import sys
+import random
+import itertools
+from functools import lru_cache
+
+
+COLORS = 'BGKRWY'  # Blue, Green, blacK, Red, White, Yellow
+SECRET = None
+
+def get_response(g):
+    if SECRET is None:
+        inp = input("%s: " % g)
+        return inp.count('+'), inp.count('-')
+    return score(SECRET, g)
+
+@lru_cache(1 << 20)
+def score(a, b):
+    matches = sum(x == y for x, y in zip(a, b))
+    return matches, sum(min(a.count(j), b.count(j)) for j in COLORS) - matches
+
+possible = [''.join(p) for p in itertools.product(COLORS, repeat=4)]
+responses = [(right, wrong) for right in range(5)
+             for wrong in range(5 - right)]
+responses.remove((3, 1))  # len(responses) = 14
+
+def guess(S):
+    if len(S) == len(possible):  # first
+        return 'BBGG'
+    if len(S) == 1:
+        return list(S)[0]
+    # Pick a guess which minimizes the maximum number of remaining S over
+    # all 14 responses.
+    # The guess will result in the minimum elements S remaining, in the
+    # next step, regardless of what the next response is.
+    worst = [max(sum(score(s, possible[i]) == resp for s in S)
+                 for resp in responses)
+             for i in range(1296)]
+    minimum = min(worst)
+    print('minimum:', minimum)
+    choices = [possible[i] for i in range(1296) if worst[i] == minimum]
+    print('choices:', len(choices))
+    return random.choice(choices)
+
+def solve():
+    S = set(possible)
+    for i in itertools.count(1):
+        print("===== guess: %d    len(S): %s  =====" % (i, len(S)))
+        g = guess(S)
+        resp = get_response(g)
+        print("%s %s" % (g, '+' * resp[0] + '-' * resp[1]))
+        if resp == (4, 0):
+            return i
+        # only keep the codes which would give the same response
+        S = set(s for s in S if score(s, g) == resp)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        sys.exit("Usage: %s [secret]" % sys.argv[0])
+
+    if len(sys.argv) == 2:
+        SECRET = sys.argv[1]
+        if SECRET == '-':
+            SECRET = ''.join(random.choices(COLORS, k=4))
+            print("secret: %s" % SECRET)
+            solve()
+            sys.exit()
+        if len(SECRET) != 4 or set(SECRET) - set(COLORS):
+            sys.exit("is not a well-formed Mastermind code: %r" % SECRET)
+
+    solve()
